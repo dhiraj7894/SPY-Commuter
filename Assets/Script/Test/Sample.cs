744abc3f83bbd9634dac87bcd11eb0783c642855
Dhirajlal Vishwakarma <47329713+dhiraj7894@users.noreply.github.com>
2020-04-28 23:23:57 +0530

    nEW uPDATE

--- Assets/Script/Test/Sample.cs
+++ Assets/Script/Test/Sample.cs
@@ -6,25 +6,23 @@ public class Sample : MonoBehaviour
 {
     public List<GameObject> bone_list = new List<GameObject>();
 
-    public float speed = 0.2f;
+    //[SerializeField]
+    private float speed = 1f;
+    public float data;
     private void Start()
     {
             foreach (Transform child in transform)
             {
                 bone_list.Add(child.gameObject);
-                foreach (Transform child1 in child)
-                {
-                    bone_list.Add(child1.gameObject);
-                }
             }
-        
-        
+
+        setPosition();
     }
 
 
-    private void Update()
+    private void setPosition()
     {
-
+        bone_list[0].transform.position = new Vector3(0, 0.11f, 0);
     }
 
 }
