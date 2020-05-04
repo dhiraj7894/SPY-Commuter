685ced4a49fd0dc391777b12795cb7606d029414
Dhirajlal Vishwakarma <47329713+dhiraj7894@users.noreply.github.com>
2020-05-04 19:54:40 +0530

    Update
    Added car animation
    Added passengers animation much more

---
+++ Assets/Script/Vehicle/Explode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    public static Explode instance;
+    public float redius = 5f, force, upForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    public void explode()
+    {
+        //Collect all collider with this sphere is colliding in array
+       Collider[] colliders =  Physics.OverlapSphere(transform.position, redius);
+        //every each collider which are present nearby get component rigidbody and apply force 
+        foreach(Collider nearbyObject in colliders)
+        {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, redius, upForce, ForceMode.Impulse);
+            }
+        }
+    }
+}
